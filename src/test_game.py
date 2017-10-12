01ef00bae256f28dc0352e149db14d2ae19958d3
Luwangel <adrien.amoros@gmail.com>
2017-10-12 11:05:20 +0200

    Fix the test

--- src/test_game.py
+++ src/test_game.py
@@ -10,4 +10,4 @@ class GameTest(unittest.TestCase):
 
     def test_build_grid(self):
         grid = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
-        self.assertEqual(buildGrid(), grid)
+        self.assertEqual(build_grid(), grid)
