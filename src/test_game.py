5ebc4e754a9c25bf624c9c847f59362c3c78bffc
Luwangel <adrien.amoros@gmail.com>
2017-10-12 11:54:55 +0200

    Fix the test

--- src/test_game.py
+++ src/test_game.py
@@ -6,8 +6,8 @@ class GameTest(unittest.TestCase):
 
     def test_start(self):
         s = 'Welcome to the 15 puzzle game.'
-        self.assertEqual(start(), s)
+        self.assertEqual(game.start(), s)
 
     def test_build_grid(self):
         grid = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
-        self.assertEqual(build_grid(), grid)
+        self.assertEqual(game.build_grid(), grid)
