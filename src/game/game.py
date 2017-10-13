9a7c2c8225ff8275b65c8a9a33bc62c314501c71
Luwangel <adrien.amoros@gmail.com>
2017-10-13 16:27:31 +0200

    Rewrite the grid build using numpy

--- src/game/game.py
+++ src/game/game.py
@@ -4,17 +4,9 @@ from game.exception import MoveException, NoEmptyTileException
 
 
 def build_grid(size=4):
-    max_size = size ** 2 - 1
-    grid = []
-    for x in range(0, size):
-        row = []
-        for y in range(0, size):
-            tile = x * size + y + 1
-            if tile > max_size:
-                tile = 0  # Corresponds to the empty box
-            row.append(tile)
-        grid.append(row)
-    return np.array(grid)
+    grid = np.arange(size ** 2).tolist()
+    grid.append(grid.pop(0))
+    return np.array(grid).reshape(4, 4)
 
 
 def find_empty_tile(grid):
